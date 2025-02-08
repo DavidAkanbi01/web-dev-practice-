b750f609933c49a80ed7604028bf3ab2c70c989b
David Akanbi <davidakanbi01@gmail.com>
2025-02-08 03:39:41 +0100

    update

---
+++ Day 1/app.js
@@ -0,0 +1,4 @@
+// Basic interactive logic (optional enhancement)
+document.querySelector('.btn-register').addEventListener('click', function () {
+  alert('Thank you for choosing Zoompark! Registration coming soon.');
+});
